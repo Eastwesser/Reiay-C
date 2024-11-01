c4e50e8866fc9030f4f60f6d1c25480502b87ba8
Dennis Mattews <alttert@mail.ru>
2024-11-02 00:30:04 +0300

    added comments for culture i18n code

--- Middlewares/LocalizationMiddleware.cs
+++ Middlewares/LocalizationMiddleware.cs
@@ -1,35 +1,45 @@
+// Подключаем пространства имен для работы с культурами, HTTP-запросами и логированием
 using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Relay.Middlewares;
 
-public class LocalizationMiddleware
+namespace Relay.Middlewares
 {
-    private readonly RequestDelegate _next;
-    private readonly RequestCultureProvider _requestCultureProvider;
-    private readonly ILogger<LocalizationMiddleware> _logger;
+    // Middleware для логирования информации о выбранной культуре и ее сохранения в текущем контексте
+    public class LocalizationMiddleware
+    {
+        // Поля для хранения делегата следующего middleware, провайдера культуры и логгера
+        private readonly RequestDelegate _next;
+        private readonly RequestCultureProvider _requestCultureProvider;
+        private readonly ILogger<LocalizationMiddleware> _logger;
 
-    public LocalizationMiddleware(
-        RequestDelegate next,
-        RequestCultureProvider requestCultureProvider,
-        ILogger<LocalizationMiddleware> logger
+        // Конструктор инициализирует параметры
+        public LocalizationMiddleware(
+            RequestDelegate next,
+            RequestCultureProvider requestCultureProvider,
+            ILogger<LocalizationMiddleware> logger
         )
-    {
-        _next = next;
-        _requestCultureProvider = requestCultureProvider;
-        _logger = logger;
-    }
+        {
+            _next = next;
+            _requestCultureProvider = requestCultureProvider;
+            _logger = logger;
+        }
 
-    public async Task InvokeAsync(HttpContext context)
-    {
-        // Определяем culture пользователя с помощью RequestCultureProvider
-        var culture = _requestCultureProvider.DetermineRequestCulture(context);
-        context.Items["RequestCulture"] = culture;
+        // Метод для обработки входящего запроса и установки культуры
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Определяем культуру запроса, используя RequestCultureProvider
+            var culture = _requestCultureProvider.DetermineRequestCulture(context);
 
-        _logger.LogInformation("Культура запроса установлена: {Culture}", culture.Name);
+            // Сохраняем информацию о культуре в контексте запроса
+            context.Items["RequestCulture"] = culture;
 
-        // Передача управления следующему middleware
-        await _next(context);
+            // Логируем установленную культуру для текущего запроса
+            _logger.LogInformation("Культура запроса установлена: {Culture}", culture.Name);
+
+            // Передача управления следующему middleware
+            await _next(context);
+        }
     }
 }
